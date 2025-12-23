c7c5be3284efcc22bd78a226d738e0967fcd637f
pgoq <97684729+pgoq@users.noreply.github.com>
2025-12-23 14:51:31 -0300

    add lower bound to bit

--- content/data-structures/Bit.h
+++ content/data-structures/Bit.h
@@ -1,14 +1,27 @@
-
+/**
+ * Description: $lower\_bound$ works the same as on vectors
+ * Time: O(\log N)
+ */
 #pragma once
 struct Bit {
-  vector<int> bit;
+  vector<ll> bit;
   Bit(int n) : bit(n + 1) {}
-  void update(int i, int v) {
+  void update(int i, ll v) {
     for (i++; i < sz(bit); i += i & -i) bit[i] += v;
   }
-  int query(int i) {
-    int ret = 0;
+  ll query(int i) {
+    ll ret = 0;
     for (i++; i > 0; i -= i & -i) ret += bit[i];
     return ret;
+  }
+  int lower_bound(ll v){ // min pos st sum of [0, pos] >= sum
+    int pos = 0;
+    for(int j=(1 << 23); j >= 1; j/=2){
+      if(pos+j < sz(bit) && bit[pos + j] < v){
+        pos += j;
+        v -= bit[pos];
+      }
+    }
+    return pos;
   }
 };
