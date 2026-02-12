18f7b123a1935e557f16558b8544c7035fc4dc68
pgoq <97684729+pgoq@users.noreply.github.com>
2026-02-12 20:24:31 -0300

    que pena

---
+++ content/various/TernarySearch.h
@@ -0,0 +1,26 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2015-05-12
+ * License: CC0
+ * Source: own work
+ * Description:
+ * Find the smallest i in $[a,b]$ that maximizes $f(i)$, assuming that $f(a) < \dots < f(i) \ge \dots \ge f(b)$.
+ * To reverse which of the sides allows non-strict inequalities, change the < marked with (A) to <=, and reverse the loop at (B).
+ * To minimize $f$, change it to >, also at (B).
+ * Usage:
+	int ind = ternSearch(0,n-1);
+ * Time: O(\log(b-a))
+ * Status: tested
+ */
+#pragma once
+
+int ternSearch(int a, int b) {
+	assert(a <= b);
+	while (b - a >= 5) {
+		int mid = (a + b) / 2;
+		if (f(mid) < f(mid+1)) a = mid; // (A)
+		else b = mid+1;
+	}
+	rep(i,a+1,b+1) if (f(a) < f(i)) a = i; // (B)
+	return a;
+}
