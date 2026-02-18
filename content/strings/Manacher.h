6e3fc6fcf1cde5789f196573114a030a43f3fef1
pgoq <97684729+pgoq@users.noreply.github.com>
2026-02-18 19:43:57 -0300

    add Simplex

--- content/strings/Manacher.h
+++ content/strings/Manacher.h
@@ -9,19 +9,16 @@
  */
 
 
-array<vector<int>, 2> manacher(const string& s) {
-    int n = sz(s);
-    array<vector<int>,2> p={vector<int>(n+1),vector<int>(n)};
-    for (int z = 0; z < 2; z++) {
-        for (int i = 0, l = 0, r = 0; i < n; i++) {
-            int t = r - i + !z;
-            if (i < r) p[z][i] = min(t, p[z][l + t]);
-            int L = i - p[z][i], R = i + p[z][i] - !z;
-            while(L >= 1 && R+1 < n && s[L-1] == s[R+1]){
-                p[z][i]++, L--, R++;
-            }
-            if (R > r) l = L, r = R;
-        }
-    }
-    return p;
+array<vi, 2> manacher(const string& s) {
+	int n = sz(s);
+	array<vi,2> p = {vi(n+1), vi(n)};
+	rep(z,0,2) for (int i=0,l=0,r=0; i<n; i++) {
+		int t = r-i+!z;
+		if (i<r) p[z][i] = min(t, p[z][l+t]);
+		int L = i-p[z][i], R = i+p[z][i]-!z;
+		while (L>=1 && R+1<n && s[L-1] == s[R+1])
+			p[z][i]++, L--, R++;
+		if (R>r) l=L, r=R;
+	}
+	return p;
 }
